dcef36cfbc0a1fb82eafa58ba17be667a76fc45c
clatthew <mattjamesclough@gmail.com>
2024-09-27 15:56:39 +0100

    implement last_child

--- src/xml_element.py
+++ src/xml_element.py
@@ -1,14 +1,35 @@
+from pickle import dump 
 class XMLElement:
-    def __init__(self, attribute=None, value=None, parent=None, root=False):
+    def __init__(self, tag : str, attribute=None, value=None, parent=None, root=False):
+        self.tag = tag
         self.attribute = attribute
         self.value = value
         self.root = root
         self.children = []
         self.parent = parent
 
-    def add_child(self, attribute=None, value=None):
-        child = XMLElement(attribute, value, parent=self)
-        self.children.append(child)
+    def add_child(self, new_child):
+        new_child.parent = self
+        new_child.root = False
+        self.children.append(new_child)
 
-    def add_sibling(self, attribute=None, value=None):
-        self.parent.add_child(attribute, value)
+    def make_child(self, tag: str, attribute=None, value=None):
+        new_child = XMLElement(tag, attribute, value)
+        self.add_child(new_child)
+
+    def make_sibling(self, tag=None, attribute=None, value=None):
+        if not tag:
+            tag = self.tag
+        new_sibling = XMLElement(tag, attribute, value)
+        self.parent.add_child(new_sibling)
+
+    def to_pickle(self, filepath=None):
+        if not filepath:
+            filepath = self.tag
+        with open (f'{filepath}.pkl', 'wb') as f:
+            dump(self, f)
+
+    @property
+    def last_child(self):
+        if self.children:
+            return self.children[-1]
--- test/test_xml_element.py
+++ test/test_xml_element.py
@@ -4,14 +4,15 @@ from src.xml_element import XMLElement
 
 @fixture(scope="function")
 def root_element():
-    return XMLElement(root=True)
+    return XMLElement('bookstore',root=True)
 
 
 class Test__init__:
     @mark.it(
-        "XMLElement is initialised with attribute, value, root, children and parent by default"
+        "XMLElement is initialised with tag, attribute, value, root, children and parent by default"
     )
     def test_default_vals(self, root_element):
+        assert "tag" in dir(root_element)
         assert "attribute" in dir(root_element)
         assert "value" in dir(root_element)
         assert "root" in dir(root_element)
@@ -19,28 +20,56 @@ class Test__init__:
         assert "parent" in dir(root_element)
 
 
-class Testadd_child:
-    @mark.it("add_child adds an instance of XMLElement to children")
+class Testmake_child:
+    @mark.it("make_child adds an instance of XMLElement to children")
     def test_add_child_instance(self, root_element):
-        root_element.add_child()
+        root_element.make_child('book')
         for child in root_element.children:
             assert isinstance(child, XMLElement)
 
-    @mark.it("add_child sets correct attribute")
+    @mark.it("make_child sets correct attribute")
     def test_add_child_attribute(self, root_element):
-        root_element.add_child(("category", "cooking"))
+        root_element.make_child('book', ("category", "cooking"))
         assert root_element.children[0].attribute == ("category", "cooking")
 
-    @mark.it("add_child sets correct value")
+    @mark.it("make_child sets correct value")
     def test_add_child_value(self, root_element):
-        root_element.add_child(value="Matthew")
+        root_element.make_child('book',value="Matthew")
         assert root_element.children[0].value == "Matthew"
 
 
 class Testadd_sibling:
     @mark.it("add_sibling adds an instance of XMLElement to parent's children")
-    def test_add_child_instance(self, root_element):
-        root_element.add_child()
-        root_element.children[0].add_sibling(value="Carrots")
+    def test_add_sibline(self, root_element):
+        root_element.make_child('book')
+        root_element.children[0].make_sibling(value="Carrots")
         assert len(root_element.children) == 2
         assert root_element.children[1].value == "Carrots"
+
+    @mark.it("add_sibling sets new sibling's tag to own value by default")
+    def test_add_subling_tag(self, root_element):
+        root_element.make_child('book')
+        root_element.last_child.make_sibling()
+        for child in root_element.children:
+            assert child.tag == 'book'
+
+class Testlast_child:
+    @mark.it('last_child returns only child')
+    def test_last_child_one_child(self, root_element):
+        test_child = XMLElement('book')
+        root_element.add_child(test_child)
+        assert root_element.last_child is test_child
+
+    @mark.it('last_child returns None if children is empty')
+    def test_last_child_no_child(self, root_element):
+        assert not root_element.last_child
+
+    @mark.it('last_child returns last child of many')
+    def test_last_child_one_child(self, root_element):
+        test_child1 = XMLElement('book')
+        test_child2 = XMLElement('book')
+        test_child3 = XMLElement('book')
+        root_element.add_child(test_child1)
+        root_element.add_child(test_child2)
+        root_element.add_child(test_child3)
+        assert root_element.last_child is test_child3
