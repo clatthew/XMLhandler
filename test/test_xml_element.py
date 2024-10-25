c12972c864ca12d95a32d6b366a81ca6e6cfffe9
clatthew <mattjamesclough@gmail.com>
2024-10-25 19:30:33 +0100

    swap apostrophe for quotation marks

--- test/test_xml_element.py
+++ test/test_xml_element.py
@@ -82,7 +82,7 @@ class Testtag:
         test_tree = XMLElement(tag_name)
         assert test_tree.tag == tag_name
 
-    @mark.it("Raises ValueError if tag name begins with \"xml\" with an underscore")
+    @mark.it('Raises ValueError if tag name begins with "xml" with an underscore')
     def test_underscore_start(self):
         tag_name = "xmlatthew"
         with raises(ValueError) as err:
