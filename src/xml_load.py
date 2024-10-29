90ab74955d00bb56f4e433b80eb21c407bc0abd1
clatthew <mattjamesclough@gmail.com>
2024-10-29 17:38:27 +0000

    add docstrings

--- src/xml_load.py
+++ src/xml_load.py
@@ -2,7 +2,14 @@ from src.xml_element import XMLElement
 from re import compile
 
 
-def get_element_from_line(line: str, entities: dict = {}):
+def get_element_from_line(line: str, entities: dict = {}) -> XMLElement | None:
+    """Create an XMLElement object from a line of an XML file.
+
+    Extract the tag_name, attributes and value of the XML element contained in line, if line contains a start tag. If line contains only a stop tag, return None.
+    Arguments:
+    line -- an arbitrary string containing XML syntax.
+    entities -- a dictionary containing user-defined entity references which will be replaced when building the XMLElement.
+    """
     non_marker_chars = '[^</>=" ]*'
     value_chars = '[^</>="]*'
     start_pattern = rf'<(?P<tag_name>{non_marker_chars}) ?(?P<attributes>({non_marker_chars}="{non_marker_chars}" ?)*)>$'
@@ -39,7 +46,13 @@ def get_element_from_line(line: str, entities: dict = {}):
     return XMLElement(tag_name, attributes, value=value)
 
 
-def remove_refs(line: str, def_refs: dict = {}):
+def remove_refs(line: str, def_refs: dict = {}) -> str:
+    """Restore a string containing XML entity references to the intended 'plain' version.
+
+    Arguments:
+    line -- an arbitrary string containing XML syntax.
+    def_refs -- a dictionary containing entity references defined at the beginning of the XML document (default empty dict).
+    """
     predef_refs = {"lt": "<", "gt": ">", "apos": "'", "quot": '"', "amp": "&"}
     refs = def_refs | predef_refs
     for ref in refs:
@@ -51,7 +64,12 @@ def remove_refs(line: str, def_refs: dict = {}):
     return line
 
 
-def extract_entities(doc_info: str):
+def extract_entities(doc_info: str) -> dict:
+    """Read the document information of an XML file and return a dictionary containing the defined entity references.
+
+    Arguments:
+    doc_info -- the beginning of an XML file containing strings like '<!ENTITY j \"l\">'.
+    """
     entity_list = [i for i in doc_info.split("<!") if i[0:6].upper() == "ENTITY"]
     entities = {}
     for element in entity_list:
@@ -64,7 +82,12 @@ def extract_entities(doc_info: str):
     return entities
 
 
-def extract_metadata(preamble: str):
+def extract_metadata(preamble: str) -> dict:
+    """Return the XML version and encoding protocol defined on the first line of the XML file.
+    
+    Arguments:
+    premble -- an arbitrary string containing XML syntax.
+    """
     metadata = {}
     for key in ["xml version", "encoding"]:
         key_start = preamble.lower().index(key)
@@ -75,7 +98,13 @@ def extract_metadata(preamble: str):
 
 
 def starts_a_new_comment(line: str) -> tuple[bool, str | None]:
-    """return True if line contains characters starting the beginning of a comment"""
+    """Return (True, str| None) if line contains characters which mark the beginning of an XML comment.
+    
+    Identify whether a line is entirely a comment, partially a commment, or not a comment at all. If the line is partially a comment, return the part of the line before the comment begins as the second part of the return.
+
+    Arguments:
+    line -- an arbitrary string containing XML syntax.
+    """
     comment_start_token = "<!--"
     if comment_start_token not in line:
         return False, None
@@ -87,6 +116,13 @@ def starts_a_new_comment(line: str) -> tuple[bool, str | None]:
 
 
 def ends_a_comment(line: str) -> tuple[bool, str | None]:
+    """Return (True, str| None) if line contains characters which mark the end of an XML comment.
+    
+    Identify whether a line is entirely a comment, partially a commment, or not a comment at all. If the line is partially a comment, return the part of the line after the comment ends as the second part of the return.
+    
+    Arguments:
+    line -- an arbitrary string containing XML syntax.
+    """
     comment_end_token = "-->"
     token_length = len(comment_end_token)
     if comment_end_token not in line:
@@ -99,6 +135,10 @@ def ends_a_comment(line: str) -> tuple[bool, str | None]:
 
 
 def generate_noncomment_lines(filepath: str):
+    """Return a generator which supplies non-comment information from the XML file.
+    
+    Arguments:
+    filepath -- location of the XML file being read"""
     with open(filepath, "r") as f:
         in_comment = False
         line = f.readline()
@@ -126,7 +166,10 @@ def generate_noncomment_lines(filepath: str):
 
 
 def load_xml_from_file(filepath: str):
-    """return XMLElement object containing information described in the XML file at filepath"""
+    """Return an XMLElement object containing information described in the XML file at the filepath given.
+    
+    Arguments:
+    filepath -- location of the XML file being read"""
 
     f = generate_noncomment_lines(filepath)
     metadata = extract_metadata(next(f))
@@ -141,6 +184,8 @@ def load_xml_from_file(filepath: str):
         line = next(f)
 
     root_element = get_element_from_line(line.strip(), entities)
+    if not isinstance(root_element, XMLElement):
+        raise TypeError(f'No parsable XML tree found at {filepath}.')
     root_element.xml_version = metadata["xml version"]
     root_element.encoding = metadata["encoding"]
     current_parent = root_element
